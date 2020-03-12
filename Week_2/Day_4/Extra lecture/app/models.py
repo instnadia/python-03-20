92a15c85542a65d73cc27c5464ef14b08dd4c776
instnadia <nnguyen@codingdojo.com>
2020-03-12 15:25:32 -0700

    extra

---
+++ Week_2/Day_4/Extra lecture/app/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+class Dojo(models.Model):
+    name = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=2)
+    desc = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"{self.city} {self.name} {self.state}"
+
+class Ninja(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    dojos = models.ForeignKey(Dojo, related_name="ninjas", on_delete=models.CASCADE)
+
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    desc = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    books = models.ManyToManyField(Book, related_name="authors") 
